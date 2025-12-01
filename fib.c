5b73ee81af7400b3302cfd61f1bbc2a1527bf983
PaleVerge <fnme370679@gmail.com>
2025-12-01 16:51:46 +0800

    fib

---
+++ fib.c
@@ -0,0 +1,26 @@
+//
+// Created by rint on 2025/11/25.
+//
+#include <stdio.h>
+int fib(int n) {
+    int f1=1,f2=1,f3;
+    if(n==1 || n==2) {
+        return 1;
+    }else {
+        for (int i = 3; i <= n; i++) {
+            f3=f1+f2;
+            f1=f2;
+            f2=f3;
+        }
+        return f3;
+    }
+}
+int main(void) {
+    int n;
+    scanf("%d",&n);
+    for (int i = 1; i <= n; i++) {
+        printf("%d ",fib(i));
+    }
+
+    return 0;
+}
